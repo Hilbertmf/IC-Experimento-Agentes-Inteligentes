cf70367cdc32633d49a0601eb0f96d87a5e0c2b6
hilbert-franca <hilbertmf@gmail.com>
2025-07-24 14:36:51 -0300

    initial commit

---
+++ AI/src/ai/worlds/vacuum/RandomVacuumAgent.java
@@ -0,0 +1,29 @@
+package ai.worlds.vacuum;
+/**
+ * A vacuum agent that simply chooses actions at random
+ * @author Jill Zimmerman -- jill.zimmerman@goucher.edu
+ *
+ */
+
+public class RandomVacuumAgent extends VacuumAgent
+{   
+	private int moveCount;
+	
+	/**
+	 * Determine the next action to be performed.
+	 */
+    public void determineAction()
+    {
+	int i = (int)Math.floor(Math.random()*5);
+	switch (i) {
+	    case 0: action = "forward"; break;
+	    case 1: action = "turn right"; break;
+	    case 2: action = "turn left"; break;
+	    case 3: action = "shut-off"; break;
+	    case 4: action = "suck";
+	}
+    }
+    public int getMoveCount() {
+    	return moveCount;
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# IC-Experimento-Agentes-Inteligentes
